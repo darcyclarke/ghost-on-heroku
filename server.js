db5c7f7999a613ca8d6c60029b646bd6ec112a49
Darcy Clarke <darcy@darcyclarke.me>
2019-01-31 15:05:38 -0500

    Modify config creation to remove unecessary configs

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const ghost = require('ghost')
 const cluster = require('cluster')
 
 // Heroku sets `WEB_CONCURRENCY` to the number of available processor cores.
-const WORKERS = process.env.WEB_CONCURRENCY || 1;
+const WORKERS = process.env.WEB_CONCURRENCY || 1
 
 if (cluster.isMaster) {
   // Master starts all workers and restarts them when they exit.
   cluster.on('exit', (worker, code, signal) => {
-    console.log(`Starting a new worker because PID: ${worker.process.pid} exited code ${code} from ${signal} signal.`);
+    console.log(`Starting a new worker because PID: ${worker.process.pid} exited code ${code} from ${signal} signal.`)
     cluster.fork()
   })
   for (var i = 0; i < WORKERS; i++) {
